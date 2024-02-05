c8ce97441b8bcbb75485c5a9f5dc5f08fa489554
Yash Raj Singh <yashrajsingh4799@gmail.com>
2024-02-05 13:46:06 +0530

    Footer UI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import APITester from "./components/APITester";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
@@ -8,8 +9,9 @@ const App: React.FC = () => {
       <header className="App-header">
         <Navbar />
         {/* <h1 className="text-3xl font-bold mb-4">CRUD Boy</h1> */}
-        <APITester />
       </header>
+      <APITester />
+      <Footer />
     </div>
   );
 };
